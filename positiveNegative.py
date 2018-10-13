9a8141ca76a107723e96bcc870a261bb5f4edf57
krnblni <karanbalani007@gmail.com>
2018-10-13 20:41:59 +0530

    added python code - beginner set1 ques1

---
+++ positiveNegative.py
@@ -0,0 +1,14 @@
+def checkNumber(number):
+    if number == 0:
+        return "Zero"
+    elif number < 0:
+        return "Negative"
+    elif number > 0:
+        return "Positive"
+
+def main():
+    number  = input("\nEnter any number to check if it is positive/negative/zero: ")
+    result = checkNumber(number)
+    print("\nThe number is {}\n".format(result))
+
+main()
